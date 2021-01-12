acd22963acf3c2f035e7738f92a20b6faa3e2982
huiylu <74116599+huiylu@users.noreply.github.com>
2021-01-12 12:02:57 -0800

    added show id

--- routes/prehistoric.js
+++ routes/prehistoric.js
@@ -1,14 +1,19 @@
+const { Console } = require('console');
 const express=require('express');
 const router=express.Router();
 const fs=require('fs');
 
+let prehistoric = fs.readFileSync('./prehistoric.json');
+let prehistoricData=JSON.parse(prehistoric);
+
 router.get('/', (req, res)=>{
     //res.send('Dino');
-    let prehistoric = fs.readFileSync('./prehistoric.json');
-    let prehistoricData=JSON.parse(prehistoric);
+    
     console.log(prehistoricData);
     res.render('prehistoric/index',{prehistorics:prehistoricData });
 });
+
+
 
 router.get('/new',(req,res)=>{
     res.render('prehistoric/new');
@@ -20,9 +25,14 @@ router.post('/',(req,res)=>{
     prehistoricJS=JSON.parse(prehistoric);
     prehistoricJS.push(req.body);
     let prehistoricJSON=JSON.stringify(prehistoricJS);
-
+    console.log(prehistoricJSON[0]);
     fs.writeFileSync('./prehistoric.json', prehistoricJSON);
     res.redirect('/prehistoric');
+});
+router.get('/:id', (req,res)=>{
+    console.log(req.params.id);
+    console.log(prehistoricData[req.params.id]);
+    res.render('prehistoric/show', {prehistorics: prehistoricData[req.params.id]});
 });
 
 module.exports = router;
