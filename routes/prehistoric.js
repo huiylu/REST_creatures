2f01de1e6e28f0ff38fae6bd65f911df44c015bb
huiylu <74116599+huiylu@users.noreply.github.com>
2021-01-12 13:37:28 -0800

    Added entry

--- routes/prehistoric.js
+++ routes/prehistoric.js
@@ -20,20 +20,24 @@ router.get('/new',(req,res)=>{
 })
 
 router.post('/',(req,res)=>{
-    let prehistoric= fs.readFileSync('./prehistoric.json');
+    //let prehistoric= fs.readFileSync('./prehistoric.json');
     console.log(req.body);
-    prehistoricJS=JSON.parse(prehistoric);
-    prehistoricJS.push(req.body);
-    let prehistoricJSON=JSON.stringify(prehistoricJS);
-    console.log(prehistoricJSON[0]);
+    prehistoricData=JSON.parse(prehistoric);
+    prehistoricData.push(req.body);
+    let prehistoricJSON=JSON.stringify(prehistoricData);
+    console.log(prehistoricJSON);
     fs.writeFileSync('./prehistoric.json', prehistoricJSON);
     res.redirect('/prehistoric');
 });
+
+
 router.get('/:id', (req,res)=>{
     console.log(req.params.id);
     console.log(prehistoricData[req.params.id]);
     res.render('prehistoric/show', {prehistorics: prehistoricData[req.params.id]});
 });
+
+
 
 module.exports = router;
 
